49f68afe116beadc98ec11604ec9664a9eeaf46f
Sven Ziereis <40876449+SvenSapphire@users.noreply.github.com>
2023-12-11 19:43:58 +0100

    Update application_sender.py

--- voxstellar/application_sender.py
+++ voxstellar/application_sender.py
@@ -7,8 +7,6 @@ import hmac
 from voxstellar.debug import Debug
 from voxstellar.config import Config
 
-SECRET_KEY = 'sexy69'
-
 
 class ApplicationSender:
     def __init__(self, voxstellar):
@@ -23,8 +21,6 @@ class ApplicationSender:
             'data': payload
         })
 
-        Debug.logger.debug("------------------------ HIT ------------------------")
-
         signature = hmac.new(key.encode('utf-8'), json_data.encode('utf-8'), hashlib.sha256)
         signature_hex = signature.hexdigest()
         headers = {
@@ -32,18 +28,9 @@ class ApplicationSender:
             'Signature': f'{signature_hex}'
         }
 
-        Debug.logger.debug("------------------------ HIT AFTER ENCRYPTION ------------------------")
-
-        Debug.logger.debug(json_data)
-        Debug.logger.debug(f'{signature_hex}')
-
-        # measure the time how long this will take
-        start = time.time()
         response = requests.post(url, data=json_data, headers=headers)
-        end = time.time()
 
         if response.status_code == 200:
             Debug.logger.debug("Webhook sent successfully.")
-            Debug.logger.debug(f"Webhook took {end - start} seconds to complete.")
         else:
             Debug.logger.debug(f"Webhook failed with status code: {response.status_code}")
