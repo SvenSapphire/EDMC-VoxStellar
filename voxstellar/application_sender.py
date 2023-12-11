d2be2f494944a9bc2542f9cc6e70f5d28df237b4
Sven Ziereis <40876449+SvenSapphire@users.noreply.github.com>
2023-12-11 18:46:45 +0100

    feat: add more debug loggers

--- voxstellar/application_sender.py
+++ voxstellar/application_sender.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import json
 import hashlib
@@ -14,7 +15,6 @@ class ApplicationSender:
         self.voxstellar = voxstellar
 
     def send(self, cmdr, payload):
-
         url = Config(self.voxstellar).api('voxstellar')['url']
         key = Config(self.voxstellar).api('voxstellar')['key']
 
@@ -23,16 +23,27 @@ class ApplicationSender:
             'data': payload
         })
 
+        Debug.logger.debug("------------------------ HIT ------------------------")
+
         signature = hmac.new(key.encode('utf-8'), json_data.encode('utf-8'), hashlib.sha256)
         signature_hex = signature.hexdigest()
         headers = {
             'Content-Type': 'application/json',
             'Signature': f'{signature_hex}'
         }
-        response = requests.post(url, data=json_data, headers=headers)
 
-        # TODO: finish response
+        Debug.logger.debug("------------------------ HIT AFTER ENCRYPTION ------------------------")
+
+        Debug.logger.debug(json_data)
+        Debug.logger.debug(f'{signature_hex}')
+
+        # measure the time how long this will take
+        start = time.time()
+        response = requests.post(url, data=json_data, headers=headers)
+        end = time.time()
+
         if response.status_code == 200:
             Debug.logger.debug("Webhook sent successfully.")
+            Debug.logger.debug(f"Webhook took {end - start} seconds to complete.")
         else:
             Debug.logger.debug(f"Webhook failed with status code: {response.status_code}")
