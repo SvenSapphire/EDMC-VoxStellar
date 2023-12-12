0a6ca41072d8cc12ed14f8b6705186ccbc275150
Sven Ziereis <40876449+SvenSapphire@users.noreply.github.com>
2023-12-12 16:35:56 +0100

    measure: request-response

--- voxstellar/application_sender.py
+++ voxstellar/application_sender.py
@@ -1,16 +1,25 @@
-import time
+import logging
+
 import requests
 import json
 import hashlib
 import hmac
-
-from voxstellar.debug import Debug
 from voxstellar.config import Config
-
 
 class ApplicationSender:
     def __init__(self, voxstellar):
         self.voxstellar = voxstellar
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        fh = logging.FileHandler('application_sender.log')
+        fh.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+
+        self.logger.addHandler(fh)
 
     def send(self, cmdr, payload):
         url = Config(self.voxstellar).api('voxstellar')['url']
@@ -21,16 +30,32 @@ class ApplicationSender:
             'data': payload
         })
 
-        signature = hmac.new(key.encode('utf-8'), json_data.encode('utf-8'), hashlib.sha256)
-        signature_hex = signature.hexdigest()
+        signature = hmac.new(key.encode('utf-8'), json_data.encode('utf-8'), hashlib.sha256).hexdigest()
         headers = {
             'Content-Type': 'application/json',
-            'Signature': f'{signature_hex}'
+            'Signature': signature,
+            'Connection': 'close'
+
         }
 
         response = requests.post(url, data=json_data, headers=headers)
 
+        request = response.request
+        self.logger.debug(f"Request URL: {request.url}")
+        self.logger.debug(f"Request Method: {request.method}")
+        self.logger.debug(f"Request Headers: {request.headers}")
+        self.logger.debug(f"Request Body: {request.body}")
+        self.logger.debug("-------------------------------- END OF REQUEST --------------------------------")
+
         if response.status_code == 200:
-            Debug.logger.debug("Webhook sent successfully.")
+            self.logger.debug("Webhook sent successfully.")
         else:
-            Debug.logger.debug(f"Webhook failed with status code: {response.status_code}")
+            self.logger.debug(f"Webhook failed with status code: {response.status_code}")
+
+        self.logger.debug(f"Response url: {response.url}")
+        self.logger.debug(f"Response content: {response.content}")
+        self.logger.debug(f"Response headers: {response.headers}")
+        self.logger.debug(f"Response reason: {response.reason}")
+        self.logger.debug(f"Response status code: {response.status_code}")
+        self.logger.debug(f"Response elapsed time: {response.elapsed}")
+        self.logger.debug(f"Response encoding: {response.encoding}")
